b6f4d2c842d0c8bd3afd95f1a6794ca841050198
Alvin Kong <alvink92@gmail.com>
2017-10-23 20:45:30 -0700

    finished making graph ds

--- graphs/alvin_kong/lib/topological_sort.rb
+++ graphs/alvin_kong/lib/topological_sort.rb
@@ -3,5 +3,20 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  sorted = []
+  top_q = []
 
+  vertices.each{ |v| top_q << v if v.in_edges.empty? }
+
+  until top_q.empty?
+    curr_v = top_q.shift
+    sorted << curr_v
+
+    curr_v.out_edges.each do |edge|
+      top_q << edge.to_vertex if edge.to_vertex.in_edges.empty?
+      edge.destroy!
+    end
+  end
+
+  sorted
 end
